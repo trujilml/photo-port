38caf723d985fc88bf34efaf87f03306f646c274
trujilml <mickeytrj@gmail.com>
2022-01-31 13:36:04 -0600

    added the nav component and updated the main app js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
-// import './App.css';
 
 function App() {
-  return (
-    <div>
-      <main>
-        <About></About>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Nav></Nav>
+            <main>
+               <About></About>
+            </main>
+        </div>
+    );
 }
 
 export default App;
