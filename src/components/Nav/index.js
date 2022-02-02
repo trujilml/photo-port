5f71f6dc0f43117d836e19c3d9f27b35929196da
trujilml <mickeytrj@gmail.com>
2022-02-02 12:30:40 -0600

    updated the nav file to allow useEffect - placed it elsewhere in order to render the page again

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,16 +2,15 @@ import React, {useEffect} from 'react';
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
-    useEffect(() => {
-        document.title = capitalizeFirstLetter(currentCategory.name);
-    // eslint-disable-next-line no-use-before-define
-    }, [currentCategory]);
-
     const {
         categories = [],
         setCurrentCategory,
         currentCategory,
     } = props;
+    
+useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+}, [currentCategory]);
 
     return (
            <header className="flex-row px-1">
