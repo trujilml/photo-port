c99f8ddcf6d3f0d5a6fe238c39214754382ed417
trujilml <mickeytrj@gmail.com>
2022-02-02 17:14:05 -0600

    ran tests on the gallery and the photolist

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,15 +3,29 @@ import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
+const categories = [
+    {name: 'portraits', description: 'portraits of people in my life'}
+]
+const mockCurrentCategory = jest.fn();
+const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe('Nav component', () => {
     it('renders', () => {
-        render(<Nav />);
+        render(<Nav 
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        />);
     });
 
     it('matches snapshot', () => {
-        const {asFragment} = render(<Nav />);
+        const {asFragment} = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            />);
 
         expect(asFragment()).toMatchSnapshot();
     });
@@ -19,7 +33,11 @@ describe('Nav component', () => {
 
 describe('emoji is visible', () => {
     it('inserts emoji into the h2', () => {
-    const { getByLabelText } = render(<Nav />);
+    const { getByLabelText } = render(<Nav 
+        categories={categories}
+        setCurrentCategory={mockSetCurrentCategory}
+        currentCategory={mockCurrentCategory}
+        />);
   
     // eslint-disable-next-line testing-library/prefer-screen-queries
     expect(getByLabelText('camera')).toHaveTextContent('📸');
@@ -28,7 +46,11 @@ describe('emoji is visible', () => {
 
 describe('links are visible', () => {
     it('inserts text into the links', () => {
-        const {getByTestId} = render(<Nav />);
+        const {getByTestId} = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+            />);
 
         // eslint-disable-next-line testing-library/prefer-screen-queries
         expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
