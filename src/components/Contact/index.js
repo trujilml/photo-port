2d8b73ba644ab93f35a11e211474256a7e416810
trujilml <mickeytrj@gmail.com>
2022-02-03 20:20:45 -0600

    validated the form data by recognizing email address recognition

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {validateEmail} from "../../utils/helpers";
 
 function ContactForm() {
 
@@ -7,9 +8,27 @@ function ContactForm() {
 
     const { name, email, message } = formState;
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     function handleChange(e) {
+        if (e.target.name === "email") {
+            const isValid = validateEmail(e.target.value);
+            if(!isValid) {
+                setErrorMessage("Your email is invalid.");
+            } else {
+                setErrorMessage("");
+            }
+        } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                    setErrorMessage("");
+                }
+        if (!errorMessage) {
         setFormState({...formState, [e.target.name]: e.target.value })
+        }
     }
+}
 
     function handleSubmit(e) {
         e.preventDefault();
