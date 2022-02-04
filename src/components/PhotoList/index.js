3396f1395c05e22df99e34744b44b2f859bcbf6b
trujilml <mickeytrj@gmail.com>
2022-02-04 16:24:41 -0600

    created and composed the modal component to show images from the gallery

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import Modal from "../Modal";
+import Modal from "../Modal";
 
 const PhotoList = ({category}) => {
 
@@ -103,14 +103,20 @@ const PhotoList = ({category}) => {
 
         const currentPhotos = photos.filter((photo) => photo.category === category);
 
+        const toggleModal = (image, i) => {
+
+        }
+
         return (
             <div>
+              <Modal />
               <div className="flex-row">
                 {currentPhotos.map((image, i) => (
                   <img
                     src={require(`../../assets/small/${category}/${i}.jpg`)}
                     alt={image.name}
                     className="img-thumbnail mx-1"
+                    onClick={() => toggleModal(image,i)}
                     key={image.name}
                   />
                 ))}
